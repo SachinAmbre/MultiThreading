0c1c0e7e1ce54268a44c966d8e5b9a42167a8a23
SachinAmbre <sachin.mbr@gmail.com>
2020-05-09 18:43:57 +0530

    Assignment 3 - Monitor

---
+++ A3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace A3
+{
+    class Program
+    {
+        static object temp = new object();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(TakeTurn);
+            t1.Name = "Ping";
+            t1.Start();
+            Thread t2 = new Thread(TakeTurn);
+            t2.Name = "Pong";
+            t2.Start();
+        }
+
+        static void TakeTurn()
+        {
+            try
+            {
+                Monitor.Enter(temp);
+                Console.WriteLine($"{Thread.CurrentThread.Name} Started");
+                Thread.Sleep(3000);
+                Console.WriteLine($"{Thread.CurrentThread.Name} Stopped");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Monitor.Exit(temp);
+            }
+        }
+    }
+}
