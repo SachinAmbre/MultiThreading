0046cb3d5d17369396814c3e3d9d6a8e041a52ca
Sachin Ambre <sachin.mbr@gmail.com>
2020-05-10 15:03:05 +0530

    Assignment 6 - Parameter Thread

---
+++ A6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace A6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 to Insert or 2 to Update");
+            int param = Convert.ToInt32(Console.ReadLine());
+            Thread th = new Thread(new ParameterizedThreadStart(UpdateDtls));
+            th.Start(param);
+
+            Console.WriteLine("Main Ends");
+        }
+
+        static void UpdateDtls(object value)
+        {
+            if (Convert.ToInt32(value) == 1)
+                //Insert in database
+                Console.WriteLine("Inserting into datbase...");
+            else
+                Console.WriteLine("Updating datbase...");
+
+            Thread.Sleep(2000);
+
+        }
+    }
+}
